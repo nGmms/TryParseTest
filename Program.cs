2414612fb9e63b228dff7aa9a6fba0a90a23fe34
Nicholas Grammatopoulos <grammatopoulos129@gmail.com>
2021-10-04 16:15:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TryParseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string numInput = Console.ReadLine();
+
+            int num = 0;
+            bool success = Int32.TryParse(numInput, out num);
+
+            if (success)
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Value Entered");
+            }
+        }
+    }
+}
